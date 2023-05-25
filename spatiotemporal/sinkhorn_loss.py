fixes: use softmax instead of sum-normalization, pivot in the right place, change to 70 epochs by default

spatiotemporal/sinkhorn_loss.py
@@ -55,9 +55,10 @@ class SinkhornLoss:
         # a_in = torch.sigmoid(a_in)
 
         # normalize a and b
-        adim = a_in.dim() - 1
-        a = a_in / torch.sum(a_in, axis=adim).unsqueeze(adim)
-        b = b_in / torch.sum(b_in, axis=adim).unsqueeze(adim)
+        a = a_in.softmax(dim=-1)
+        b = b_in.softmax(dim=-1)
+        # previous version: normalize by dividing by sum
+        # / torch.sum(a_in, axis=adim).unsqueeze(adim)
 
         # check if we predicted several steps ahead
         steps_ahead = a.size()[1]
@@ -70,7 +71,7 @@ class SinkhornLoss:
             loss = torch.mean(result, dim=0)
         else:
             loss = self.loss_object(a, self.dummy_locs, b, self.dummy_locs)
-        return torch.sum(loss)
+        return torch.sum(loss) * 1e5
 
 
 class CombinedLoss:
@@ -81,7 +82,7 @@ class CombinedLoss:
 
     def __call__(self, a_in, b_in):
         mse_loss = self.standard_mse(a_in, b_in)
-        sink_loss = self.sinkhorn_error(a_in, b_in) * 1e5
+        sink_loss = self.sinkhorn_error(a_in, b_in)
         return (1 - self.dist_weight) * mse_loss + self.dist_weight * sink_loss
 
 
@@ -91,9 +92,8 @@ class DistributionMSE:
 
     def __call__(self, a_in, b_in):
         # normalize a and b
-        adim = a_in.dim() - 1
-        a = a_in / torch.sum(a_in, axis=adim).unsqueeze(adim)
-        b = b_in / torch.sum(b_in, axis=adim).unsqueeze(adim)
+        a = a_in.softmax(dim=-1)
+        b = b_in.softmax(dim=-1)
 
         # apply standard MSE
         return self.standard_mse(a, b)

spatiotemporal/test.py
@@ -33,7 +33,7 @@ warnings.filterwarnings("ignore")
 np.random.seed(42)
 
 
-def clean_single_pred(pred, pred_or_gt="pred"):
+def clean_single_pred(pred, pred_or_gt="pred", clip=True, apply_exp=False):
     result_as_df = pred.pd_dataframe().swapaxes(1, 0).reset_index()
     result_as_df.rename(
         columns={c: i for i, c in enumerate(result_as_df.columns[1:])},
@@ -43,7 +43,10 @@ def clean_single_pred(pred, pred_or_gt="pred"):
         {"component": "group", "value": pred_or_gt, "timeslot": "steps_ahead"},
         axis=1,
     )
-    result_as_df[pred_or_gt].clip(0, MAX_RENTALS, inplace=True)
+    if apply_exp:
+        result_as_df[pred_or_gt] = np.exp(result_as_df[pred_or_gt])
+    if clip:
+        result_as_df[pred_or_gt].clip(0, MAX_RENTALS, inplace=True)
     return result_as_df
 
 
@@ -59,6 +62,20 @@ def load_data(in_path_data, in_path_stations, pivot=False):
     # # reduce demand matrix shape
     # demand_agg = demand_agg[stations_included]
     # print(demand_agg.shape)
+    # pivot if necessary
+    if "station_id" in demand_df.columns:
+        print("pivoting")
+        demand_df = demand_df.pivot(
+            index="timeslot", columns="station_id", values="count"
+        ).fillna(0)
+        demand_df = (
+            demand_df.reset_index()
+            .rename_axis(None, axis=1)
+            .set_index("timeslot")
+        )
+    else:
+        demand_df.set_index("timeslot", inplace=True)
+    print("Demand matrix", demand_df.shape)
     return demand_df, stations_locations
 
 
@@ -153,8 +170,10 @@ def test_models(
         else:
             pred = pred_raw
 
-        # transform to flat df
-        result_as_df = clean_single_pred(pred)
+        # Clean: (transform to df, clip, etc)
+        # if loss function is just distribution, we apply exp to the results
+        apply_exp = kwargs["x_loss_function"] in ["sinkhorn", "distribution"]
+        result_as_df = clean_single_pred(pred, clip=True, apply_exp=apply_exp)
         # add info about val sample
         result_as_df["val_sample_ind"] = val_sample - train_cutoff
         model_res_dfs.append(result_as_df)
@@ -195,21 +214,6 @@ if __name__ == "__main__":
             fillna_value=0,
         )
     else:
-        # pivot if necessary
-        if "station_id" in demand_agg.columns:
-            print("pivoting")
-            demand_agg = demand_agg.pivot(
-                index="timeslot", columns="station_id", values="count"
-            ).fillna(0)
-            demand_agg = (
-                demand_agg.reset_index()
-                .rename_axis(None, axis=1)
-                .set_index("timeslot")
-            )
-        else:
-            demand_agg.set_index("timeslot", inplace=True)
-        print("Demand matrix", demand_agg.shape)
-
         shared_demand_series = TimeSeries.from_dataframe(
             demand_agg, freq="1h", fillna_value=0
         )