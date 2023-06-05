3bf746760b09207166640f30bc12a3da35a0fa75
NinaWie <wnina@student.ethz.ch>
2023-06-05 23:40:52 +0200

    add emd evaluation

---
+++ spatiotemporal/emd_eval.py
@@ -0,0 +1,150 @@
+from typing import Any
+import wasserstein
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import cdist
+from scipy.special import softmax
+
+"""
+MODES:
+station_to_station: We have a prediction per group,
+but split it over the included stations in equal parts,
+then compute emd with cdist station-station
+group_to_station: We have a prediction per group, we just
+take the center coordinates of the group and use the
+distances between group centers and stations as cdist
+group_to_group: We just compute the EMD error in terms of
+rebalancing between the clusters
+"""
+
+
+def hierarchy_to_df(res_hierarchy):
+    # convert to df
+    station_group_df = []
+    for group, stations_in_group in res_hierarchy.items():
+        if group == "total":
+            continue
+        df = pd.DataFrame()
+        df["station"] = stations_in_group
+        df["group"] = group
+        df["nr_stations"] = len(stations_in_group)
+        station_group_df.append(df)
+    station_group_df = pd.concat(station_group_df).sort_values("station")
+    return station_group_df
+
+
+class EMDWrapper:
+    def __init__(self, stations, gt_reference):
+        self.gt_reference = gt_reference.set_index(
+            ["val_sample_ind", "steps_ahead"]
+        ).rename({"group": "station"}, axis=1)
+        self.stations = stations
+        assert self.stations.index.name == "station_id"
+        self.stations.sort_index(inplace=True)
+        # default dist matrix: between stations
+        self.dist_matrix = cdist(
+            self.stations[["x", "y"]], self.stations[["x", "y"]]
+        )
+
+    def __call__(self, res, res_hierarchy, mode="station_to_station"):
+        if mode == "station_to_station":
+            return self.emd_station_to_station(res, res_hierarchy)
+        elif mode == "group_to_group":
+            return self.emd_group_to_group(res, res_hierarchy)
+        elif mode == "group_to_station":
+            return self.emd_group_to_station(res, res_hierarchy)
+        else:
+            raise ValueError("Invalid mode")
+
+    def emd_station_to_station(
+        self, res: pd.DataFrame, res_hierarchy: dict = None
+    ) -> list:
+        if res_hierarchy is None:
+            # only possibility is station-to-station mode,
+            # because there are no groups
+            res_per_station = res.copy()
+            res_per_station["pred_emd"] = res_per_station["pred"]
+            res_per_station["station"] = res_per_station["group"]
+        else:
+            station_group_df = hierarchy_to_df(res_hierarchy)
+            # merge them in order to get predictions per station
+            res_per_station = pd.merge(
+                station_group_df,
+                res,
+                how="right",
+                left_on="group",
+                right_on="group",
+            )
+            res_per_station["pred_emd"] = (
+                res_per_station["pred"] / res_per_station["nr_stations"]
+            )
+        # compute EMD
+        return self.compute_emd(res_per_station)
+
+    def get_coords_per_group(self, res_hierarchy: dict) -> list:
+        # get groups
+        station_group_df = hierarchy_to_df(res_hierarchy)
+        coords_per_group = self.stations.merge(
+            station_group_df, left_index=True, how="left", right_on="station"
+        )
+        coords_per_group = coords_per_group.groupby("group").agg(
+            {"x": "mean", "y": "mean"}
+        )
+        coords_per_group.sort_index(inplace=True)
+        return coords_per_group
+
+    def emd_group_to_station(
+        self, res: pd.DataFrame, res_hierarchy: dict
+    ) -> list:
+        if res_hierarchy is None:
+            return self.emd_station_to_station(res, res_hierarchy)
+        coords_per_group = self.get_coords_per_group(res_hierarchy)
+        # distance matrix is between groups (pred) and stations (gt)
+        self.dist_matrix = cdist(
+            coords_per_group[["x", "y"]], self.stations[["x", "y"]]
+        )
+        # predictions are just the per-group predictions
+        res["pred_emd"] = res["pred"].copy()
+        res["station"] = res["group"]
+        # compute EMD
+        return self.compute_emd(res)
+
+    def emd_group_to_group(self, res: pd.DataFrame, res_hierarchy: dict):
+        if res_hierarchy is None:
+            return self.emd_station_to_station(res, res_hierarchy)
+        coords_per_group = self.get_coords_per_group(res_hierarchy)
+        # distance matrix is between groups
+        self.dist_matrix = cdist(
+            coords_per_group[["x", "y"]], coords_per_group[["x", "y"]]
+        )
+        # the prediction is just the per group prediction
+        res["pred_emd"] = res["pred"]
+        res["station"] = res["group"]
+        # the gt is in the res
+        self.gt_reference = res.set_index(["val_sample_ind", "steps_ahead"])
+
+        return self.compute_emd(res)
+
+    def compute_emd(self, res_per_station: pd.DataFrame) -> list:
+        # compute emd
+        emd = []
+        for (val_sample, steps_ahead), sample_df in res_per_station.groupby(
+            ["val_sample_ind", "steps_ahead"]
+        ):
+            sample_df = sample_df.sort_values("station")
+            pred_vals = softmax(sample_df["pred_emd"].values)
+            # get corresponding ground truth df
+            gt_df = self.gt_reference.loc[val_sample, steps_ahead].sort_values(
+                "station"
+            )
+            gt_vals = softmax(gt_df["gt"].values)
+            # compute wasserstein
+            was = wasserstein.EMD()
+            emd_distance = was(
+                pred_vals,
+                gt_vals,
+                self.dist_matrix,
+            )
+            emd.append(emd_distance)
+
+        return emd
--- spatiotemporal/visualization.py
+++ spatiotemporal/visualization.py
@@ -1,5 +1,10 @@
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
+import json
+import seaborn as sns
+
+from emd_eval import EMDWrapper
 
 
 def plot_error_evolvement(error_evolvement, out_path=None):
@@ -24,3 +29,56 @@ def plot_error_evolvement(error_evolvement, out_path=None):
         plt.show()
     else:
         plt.savefig(out_path)
+
+
+def plot_emd(path, out_path="figures"):
+    subset = [f for f in os.listdir(path) if "150" in f and "csv" in f]
+    subset_name_mapping = {
+        "1_24_1_nhits_multi_70_3_3_1_basic_1_150_kmeans": "Kmeans hierarchical\n reconciled",
+        "0_24_1_nhits_multi_70_3_3_0_basic_1_150_agg": "agglomerative",
+        "0_24_1_nhits_multi_70_3_3_0_combined_sinkhorn_1_150_kmeans": "sinkhorn\n (Kmeans)",
+        "1_24_1_nhits_multi_70_3_3_0_basic_1_150_kmeans": "Kmeans \n hierarchical",
+        "0_24_1_nhits_multi_70_3_3_0_crossentropy_1_150_agg": "cross-entropy\n (agglomerative)",
+        "0_24_1_nhits_multi_70_3_3_0_distribution_1_150_agg": "distribution \n(agglomerative)",
+        "0_24_1_nhits_multi_70_3_3_0_basic_1_150_kmeans": "Kmeans",
+    }
+
+    gt_file = [f for f in os.listdir(path) if "None" in f][0]
+    # load gt as reference (per station gt needed for evaluation)
+    gt_reference = pd.read_csv(os.path.join(path, gt_file)).drop("pred", axis=1)
+    # load stations
+    stations = (
+        pd.read_csv("data/bikes_montreal/test_stations.csv")
+        .sort_values("station_id")
+        .set_index("station_id")
+    )
+
+    emd_res_dict = {}
+    for f in subset:
+        print(f)
+        res = pd.read_csv(os.path.join(path, f))
+        with open(
+            os.path.join(path, f[:-4] + "_hierarchy.json"), "r"
+        ) as infile:
+            res_hierarchy = json.load(infile)
+
+        if f.startswith("1"):
+            # reduce to groups
+            res = res[res["group"].str.contains("Group")]
+
+        emd_compute = EMDWrapper(stations, gt_reference)
+        emd_vals = emd_compute(res, res_hierarchy, mode="group_to_group")
+        emd_res_dict[subset_name_mapping[f[:-4]]] = emd_vals
+
+    # simplify the data (just plot the average)
+    emd_plot_df = pd.DataFrame(
+        pd.DataFrame(emd_res_dict).mean(), columns=["EMD"]
+    ).sort_values("EMD")
+    emd_plot_df.index.name = "Method"
+    emd_plot_df.reset_index(inplace=True)
+
+    # plot
+    plt.figure(figsize=(20, 5))
+    sns.barplot(data=emd_plot_df, x="Method", y="EMD")
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_path, "emd_comparison.pdf"))
